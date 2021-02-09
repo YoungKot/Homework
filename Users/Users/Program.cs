47808e87e6e350a6c90854a05a549913ddd74b5a
Daniils <daniils.korolenko@if.lv>
2021-02-09 16:28:44 +0200

    Removed all the irrelevant code

--- Users/Users/Program.cs
+++ Users/Users/Program.cs
@@ -12,24 +12,9 @@ namespace Users
 {
     class Program
     {
-        public static PathConstructor path = new PathConstructor();
-        public static CSVReader reader = new CSVReader();
-        public static UserAmounts amount = new UserAmounts();
-        public static CSVWriter writer = new CSVWriter();
-        public static Result result = new Result();
-        public static Validator validator = new Validator(writer, reader, path, result);
         static void Main(string[] args)
         {
-            //string pathRead = "CSV//UserData.csv";
-            //string directory = "CSV";
-            //string fileName = "UserCount.csv";
-            //var list = reader.GetTotal(path.pathConstructor(pathRead)).Item1;
-            //int total = amount.GetTotalUsersCount(list);
-            //int lvs = amount.GetUsersByCountryCount("LV", list);
-            //int lts = amount.GetUsersByCountryCount("LT", list);
-            //int ests = amount.GetUsersByCountryCount("EE", list);
-            //validator.ValidateFile(directory,fileName,total,lvs,lts,ests);
-            //Console.ReadKey();
+            
         }
     }
 }
